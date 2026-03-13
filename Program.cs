b869663b0126d6630d4bd8905138129f70cefd53
kMojalefa <datallmjakes@gmail.com>
2026-03-13 19:52:02 +0200

    Added chatbot welcome message and basic console interface

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PROG6221POEPART1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================");
+            Console.WriteLine("   CYBERSECURITY CHATBOT SYSTEM  ");
+            Console.WriteLine("=================================");
+            Console.WriteLine();
+
+            Console.WriteLine("Welcome to the Cybersecurity Awareness Chatbot.");
+            Console.WriteLine("This program provides tips on staying safe online.");
+            Console.WriteLine();
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
